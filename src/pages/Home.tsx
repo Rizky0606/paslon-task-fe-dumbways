e30e1a7347617b4d938c2de761921a0a5d032826
Rizky Fauzi <rizkyfauzi132@gmail.com>
2023-10-05 08:01:29 +0700

    added background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -79,6 +79,8 @@ const Home = () => {
   };
 
   const handleSubmitPaslon = () => {
+    setDataPaslon("");
+    setNameVoter("");
     alert(`Name: ${nameVoter}
 Paslon: ${dataPaslon}`);
   };
@@ -98,24 +100,24 @@ Paslon: ${dataPaslon}`);
   return (
     <div className="w-full">
       {/* PASLON */}
-      <div className="grid px-14 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid px-14 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
         {users?.map((paslon: DataUsers) => {
           return (
             <div
-              className="bg-slate-400 m-5 rounded-md hover:scale-110"
+              className=" m-5 backdrop-opacity-10 bg-white/30 rounded-md hover:scale-110 "
               key={paslon.id}
             >
               <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 py-10 lg:mx-0 lg:max-w-none ">
                   <article className="flex max-w-xl flex-col items-start justify-between">
-                    <div className="group relative">
-                      <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <div className="group relative ">
+                      <h3 className="mt-3 leading-6 text-white font-bold text-[30px] group-hover:text-gray-600">
                         <p>
                           <span className="absolute inset-0"></span>
                           {paslon.name}
                         </p>
                       </h3>
-                      <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                      <p className="mt-5 line-clamp-3 text-lg leading-6 text-gray-800">
                         {paslon.visi}
                       </p>
                     </div>
@@ -145,13 +147,18 @@ Paslon: ${dataPaslon}`);
 
       <div className="w-full flex flex-wrap">
         {/* COUNTER VOTES */}
-        <div className="flex-1 m-10 bg-neutral-300 p-7 rounded-md">
+        <div className="flex-1 m-10 backdrop-opacity-10 backdrop-invert bg-white/30 p-7 rounded-md">
           <div className="flex flex-col items-start">
-            <p className="font-bold text-[30px] m-auto">Suara Saat Ini</p>
+            <p className="font-bold text-white text-[30px] m-auto">
+              Suara Saat Ini
+            </p>
             <ul>
               {users?.map((paslon: DataUsers) => {
                 return (
-                  <li key={paslon.id} className="my-5 text-[20px] font-bold">
+                  <li
+                    key={paslon.id}
+                    className="my-5 text-white text-[20px] font-bold"
+                  >
                     {paslon.name} :
                   </li>
                 );
@@ -161,7 +168,7 @@ Paslon: ${dataPaslon}`);
         </div>
 
         {/* FORM VOTE */}
-        <div className="flex-1 m-10 bg-neutral-300 rounded-md">
+        <div className="flex-1 m-10 backdrop-opacity-10 backdrop-invert bg-white/30 text-white rounded-md">
           <div className="flex flex-col px-7 py-5">
             <label htmlFor="inputName" className="text-[20px]">
               Masukan Nama
@@ -170,17 +177,17 @@ Paslon: ${dataPaslon}`);
               className="w-72 my-3 h-10 rounded-md px-5"
               type="text"
               id="inputName"
+              value={nameVoter}
               placeholder="Input Name"
               onChange={handleChangeVoterName}
             />
           </div>
           <div className="flex mx-7 py-3">
-            <div className="form-control">
+            <div className="form-control ">
               {users?.map((paslon: DataUsers) => {
                 return (
-                  <div key={paslon.id}>
+                  <div key={paslon.id} className="flex">
                     <label className="label cursor-pointer">
-                      <span className="label-text">{paslon.name}</span>
                       <input
                         type="radio"
                         name="radio-10"
@@ -188,6 +195,9 @@ Paslon: ${dataPaslon}`);
                         className="radio radio-info mx-5"
                         value={paslon.name}
                       />
+                      <span className="label-text text-white font-semibold text-[20px]">
+                        {paslon.name}
+                      </span>
                     </label>
                   </div>
                 );
